30a963c7111717f551045dd9b569a74d0acd81d7
Yorelisa Calcano <ycalcano@gmail.com>
2023-04-30 20:55:03 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'dbName': 'demo'
+};
+// exporting an object 
